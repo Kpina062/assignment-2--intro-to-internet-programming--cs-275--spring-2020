af41f37fd825183b43a79167c0c16a2a29bac565
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-04-17 13:50:55 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,12 @@
-const { src, dest } = require (`gulp`);
-const
+const { src, dest } = require(`gulp`);
+const htmlCompressor = require(`gulp-htmlmin`);
+
+let compressHTML = () => {
+    return src(`uncompressed-html*.html`)
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`compressed-html/`));
+
+};
+
+exports.compressHTML = compressHTML;
+
