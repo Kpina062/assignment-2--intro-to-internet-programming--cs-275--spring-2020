5a2fcf92adb5319839aa788d7247c4204d7d6b17
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-04-17 14:55:19 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -161,6 +161,26 @@ let serve = () => {
 
     };
 
+async function listTasks () {
+    let exec = require(`child_process`).exec;
+
+    exec(`gulp --tasks`, function (error, stdout, stderr) {
+        if (null !== error) {
+            process.stdout.write(`An error was likely generated when invoking ` +
+                `the “exec” program in the default task.`);
+        }
+
+        if (`` !== stderr) {
+            process.stdout.write(`Content has been written to the stderr stream ` +
+                `when invoking the “exec” program in the default task.`);
+        }
+
+        process.stdout.write(`\n\tThis default task does ` +
+            `nothing but generate this message. The ` +
+            `available tasks are:\n\n${stdout}`);
+    });
+}
+
 exports.safari = series(safari, serve);
 exports.firefox = series(firefox, serve);
 exports.chrome = series(chrome, serve);
