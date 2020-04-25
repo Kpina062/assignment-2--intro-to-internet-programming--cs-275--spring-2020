67e969e15b44d70be9d9ed693e4053f65d4493c0
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-04-24 22:00:10 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-function on_change(el)
-{
-    if (el.options[el.selectedIndex].value == `1`) {
+const on_change = (e1) {
+    if (el.options[e1.selectedIndex].value == `1`) {
         document.getElementById(`Recipe1`).style.display = `block`;
     } else {
         document.getElementById(`Recipe1`).style.display = `none`;
