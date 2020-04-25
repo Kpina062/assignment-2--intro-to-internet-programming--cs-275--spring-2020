d9f0fcec9ac613361abacf14b3166bacfff0528b
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-04-24 20:48:14 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-alert(`JavaScript works!`);
-
-
-
+function on_change(el)
+{
+    if (el_options[el.selectedIndex].value == '1') {
+        document.getElementById('Recipe1').style.display = 'block';
+    } else {
+        document.getElementById('Recipe1').style.display = 'none';
+    }
+    if (el_options[el.selectedIndex].value == '2') {
+        document.getElementById('Recipe2').style.display = 'block';
+    } else {
+        document.getElementById('Recipe2').style.display = 'none';
+    }
+}
