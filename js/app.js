e9f2632c0af44275903905924d9cdf0d5312e55a
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-04-25 14:57:13 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 alert(`JavaScript works!`);
 function on_change (el) {
-    if (el.options[el.selectedIndex].value === `One`) {
+    if (el.options[el.selectedIndex].value === `1`) {
         document.getElementById(`Recipe1`).style.display = `block`;
     } else {
         document.getElementById(`Recipe1`).style.display = `none`;
     }
-    if (el.options[el.selectedIndex].value !== `Two`) {
+    if (el.options[el.selectedIndex].value !== `2`) {
         document.getElementById(`Recipe2`).style.display = `block`;
     } else {
         document.getElementById(`Recipe2`).style.display = `block`;
