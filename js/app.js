1c47191374201864fa8e8610af7811c139c5a751
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-04-26 21:12:38 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,11 +2,12 @@ function on_change (el) {
     if (el.options[el.selectedIndex].value === `1`) {
         document.getElementById(`Recipe1`).style.display = `block`;
     } else {
-        document.getElementById(`Recipe1`).style.display = `none`;
+        document.getElementById(`Recipe1`).style.display = `block`;
     }
-    if (el.options[el.selectedIndex].value !== `2`) {
+    if (el.options[el.selectedIndex].value == `2`) {
         document.getElementById(`Recipe2`).style.display = `block`;
-    } else {
+    }
+    else {
         document.getElementById(`Recipe2`).style.display = `block`;
     }
 }
